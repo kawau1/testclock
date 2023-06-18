b6da84220cd14797b37e9a106d30a4a48284a099
kawaguchiyuichi <intros-toying-0s@icloud.com>
2023-06-18 15:22:50 +0900

    Initial Commit

---
+++ testclock/testclockApp.swift
@@ -0,0 +1,17 @@
+//
+//  testclockApp.swift
+//  testclock
+//
+//  Created by 川口愉一 on 2023/06/18.
+//
+
+import SwiftUI
+
+@main
+struct testclockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
